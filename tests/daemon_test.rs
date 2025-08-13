dd92f9757ac7de2a7c9b5a4d36470f29a4271669
pepedinho <2spii94@gmail.com>
2025-08-13 17:51:03 +0200

    add unit test for handle_ functions

--- tests/daemon_test.rs
+++ tests/daemon_test.rs
@@ -1,13 +1,23 @@
-#![cfg(test)]
-use std::env::temp_dir;
+use std::{collections::HashMap, env::temp_dir, sync::Arc};
 
-use core_lib::{core, logging::Logger};
+use core_lib::{
+    config::parser::ProjectConfig,
+    core::{
+        self,
+        state::{AppState, init_watch_file, remove_watch_by_id},
+        watcher::WatchContext,
+    },
+    git::repo::Repo,
+    ipc::server::{DaemonResponse, handle_list_watches, handle_rm_watch, handle_up_watch},
+    logging::Logger,
+};
 use pretty_assertions::assert_eq;
-use tokio::fs;
+use tokio::{fs, sync::RwLock};
 
 #[test]
 fn test_id_generation() {
     let res = core::id::short_id();
+    println!("generate id => {res}");
     println!("generate id => {res}");
     assert_eq!(res.len(), 12);
 }
@@ -66,5 +76,183 @@ async fn test_concurent_log_writes() -> anyhow::Result<()> {
         assert!(contents.contains(&format!("message {i}")));
     }
 
+    Ok(())
+}
+
+// #[tokio::test]
+// async fn test_handle_stop_watch_existing() -> anyhow::Result<()> {
+//     init_watch_file().await?;
+//     let id = "watch_stop".to_string();
+
+//     let mut map = HashMap::new();
+//     let ctx = WatchContext {
+//         paused: false,
+//         project_dir: "dir".to_string(),
+//         branch: "main".to_string(),
+//         repo: Repo {
+//             branch: "main".to_string(),
+//             last_commit: "abc".to_string(),
+//             name: "name".to_string(),
+//             remote: "".to_string(),
+//         },
+//         id: id.clone(),
+//         config: ProjectConfig::default(),
+//     };
+
+//     map.insert(id.clone(), ctx);
+//     let state = Arc::new(AppState {
+//         watches: RwLock::new(map),
+//     });
+
+//     let response = handle_stop_watch(state.clone(), id.clone()).await;
+//     remove_watch_by_id(&id).await?;
+
+//     match response {
+//         DaemonResponse::Success(msg) => {
+//             assert!(msg.contains("Watch stopped"));
+//         }
+//         _ => panic!("Expected success response"),
+//     }
+//     Ok(())
+// }
+
+#[tokio::test]
+async fn test_handle_up_watch_existing() -> anyhow::Result<()> {
+    init_watch_file().await?;
+    let id = "watch_up".to_string();
+
+    let mut map = HashMap::new();
+    let ctx = WatchContext {
+        paused: true, // on part d'un état "pause"
+        project_dir: "dir".to_string(),
+        branch: "main".to_string(),
+        repo: Repo {
+            branch: "main".to_string(),
+            last_commit: "abc".to_string(),
+            name: "name".to_string(),
+            remote: "".to_string(),
+        },
+        id: id.clone(),
+        config: ProjectConfig::default(),
+    };
+
+    map.insert(id.clone(), ctx);
+    let state = Arc::new(AppState {
+        watches: RwLock::new(map),
+    });
+
+    println!("before handle watch");
+    let response = handle_up_watch(state.clone(), id.clone()).await;
+    println!("handle watch dont throw up");
+    remove_watch_by_id(&id).await?;
+    println!("remove watch throw up");
+
+    match response {
+        DaemonResponse::Success(msg) => {
+            assert!(msg.contains("Watch up"));
+        }
+        _ => panic!("Excpected succes response"),
+    }
+
+    Ok(())
+}
+
+// #[tokio::test]
+// async fn test_handle_rm_watch_existing() -> anyhow::Result<()> {
+//     init_watch_file().await?;
+//     let id = "rm_watch".to_string();
+
+//     let mut map = HashMap::new();
+//     let ctx = WatchContext {
+//         paused: false,
+//         project_dir: "dir".to_string(),
+//         branch: "main".to_string(),
+//         repo: Repo {
+//             branch: "main".to_string(),
+//             last_commit: "abc".to_string(),
+//             name: "name".to_string(),
+//             remote: "".to_string(),
+//         },
+//         id: id.clone(),
+//         config: ProjectConfig::default(),
+//     };
+//     map.insert(id.clone(), ctx);
+
+//     let state = Arc::new(AppState {
+//         watches: RwLock::new(map),
+//     });
+
+//     let response = handle_rm_watch(state.clone(), id.clone()).await;
+
+//     match response {
+//         DaemonResponse::Success(msg) => assert!(msg.contains("was deleted")),
+//         _ => panic!("Expected succes response"),
+//     }
+//     Ok(())
+// }
+
+#[tokio::test]
+async fn test_handle_rm_non_existing() -> anyhow::Result<()> {
+    let state = Arc::new(AppState {
+        watches: RwLock::new(HashMap::new()),
+    });
+
+    let response = handle_rm_watch(state.clone(), "unknown".to_string()).await;
+
+    match response {
+        DaemonResponse::Error(msg) => assert!(msg.contains("ID not found")),
+        _ => panic!("Expected error response"),
+    }
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_handle_list_watches_existing() -> anyhow::Result<()> {
+    let mut map = HashMap::new();
+    map.insert(
+        "watch1".to_string(),
+        WatchContext {
+            paused: false,
+            project_dir: "dir".to_string(),
+            branch: "main".to_string(),
+            repo: Repo {
+                branch: "main".to_string(),
+                last_commit: "abc".to_string(),
+                name: "name".to_string(),
+                remote: "".to_string(),
+            },
+            id: "watch1".to_string(),
+            config: ProjectConfig::default(),
+        },
+    );
+
+    let state = Arc::new(AppState {
+        watches: RwLock::new(map),
+    });
+
+    let response = handle_list_watches(state.clone(), false).await;
+
+    match response {
+        DaemonResponse::ListWatches(list) => {
+            assert_eq!(list.len(), 1);
+            assert_eq!(list[0].id, "watch1");
+        }
+        _ => panic!("Expected list watches"),
+    }
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_handle_list_watches_empty() -> anyhow::Result<()> {
+    let state = Arc::new(AppState {
+        watches: RwLock::new(HashMap::new()),
+    });
+
+    let response = handle_list_watches(state.clone(), false).await;
+
+    match response {
+        DaemonResponse::ListWatches(list) => assert!(list.is_empty()),
+        _ => panic!("Expected empty list"),
+    }
     Ok(())
 }
